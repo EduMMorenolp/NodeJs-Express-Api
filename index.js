5c660a57ceaedd133fb531d24614fe6cc9a90b96
Edu Moreno <e.m.morenolp@gmail.com>
2024-03-07 00:00:10 -0300

    pruebas

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
-
+const pagina = require("./pagina.js");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + `/public/index.html`);
+});
+
 app.get("/productos", (req, res) => {
-    console.log("Holas")
-    res.send(`Lista de productos`);
+    res.send(pagina("Server Local"));
 });
 
 app.post("/productos", (req, res) => {
     console.log(req);
     res.send("Agregar producto");
 });
-
-
 
 app.listen(port, () => {
     console.log(`Server started on port http://localhost:${port}`);
---
+++ pagina.js
@@ -0,0 +1,12 @@
+
+
+
+const pagina = (props) => {
+    return `
+    <div>
+        <h1>Hola mundo desde ${props}</h1>
+        <button><a href="/productos">Boton</a></button>
+    </div>`;
+}
+
+module.exports = pagina;
